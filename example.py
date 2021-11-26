4f86593d2ba51161c50d76f722c933616165fc12
xxparthparekhxx <79994063+xxparthparekhxx@users.noreply.github.com>
2021-11-26 17:54:26 +0530

    added basic example

--- example.py
+++ example.py
@@ -2,9 +2,8 @@ import os
 from pywhat_auto import pywhat_auto
 
 if __name__ == "__main__":   
-    names = ["Chota Mannia"]
-    #example 
-    bot = pywhat_auto(waitingtime= 3)
+    names = ["ADD YOUR Whatsapp contact name","name2","name3"]
+    bot = pywhat_auto(waitingtime= 3) #add a delay that works on your machine
     for ele in names:
-        bot.sendfile(ele,os.path.join(os.getcwd(),'example.txt'),count= 10)
+        bot.sendfile(ele,os.path.join(os.getcwd(),'example.txt'),count= 10) #sends files 
 
